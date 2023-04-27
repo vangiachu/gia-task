b1be4025035f60c348ded50555ca5acd14a3f3e2
Chu Van Gia <vangiachu@gmail.com>
2023-04-27 23:06:32 +0700

    commit toi 27.4.2023

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,9 @@ private authService: AuthService,
   createUser(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.createUser(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string}> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,10 +4,13 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]),
+  AuthModule,
+],
   controllers: [TasksController],
   providers: [TasksService],
 })
